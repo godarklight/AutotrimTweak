cf03622b1831b7971afb77a0ac8601f5648a6be0
Christopher Andrews <godarklight@gmail.com>
2021-12-21 00:24:49 +1000

    It works

--- AutotrimTweak/AutotrimTweakFBW.cs
+++ AutotrimTweak/AutotrimTweakFBW.cs
@@ -3,24 +3,28 @@ using UnityEngine;
 using UI;
 using BalsaCore;
 using FSControl;
-
+using System.IO;
+using DirectLineRendering;
 namespace AutotrimTweak
 {
     public class AutotrimTweakFBW : IFlyByWire
     {
         PID32 pitchPID;
-        float lastPitchInput;
         float lastPitchPIDOutput;
         PID32 rollPID;
-        float lastRollInput;
         float lastRollPIDOutput;
         float lastTime;
 
         public void OnRegistered(FBWHostBase host)
         {
             lastTime = Time.realtimeSinceStartup;
-            pitchPID = new PID32(0.1f, 0.01f, 0f, -0.2f, 0.2f);
-            rollPID = new PID32(0.1f, 0.01f, 0f, -0.2f, 0.2f);
+            //300 degrees per second is full swing
+            float maxValue = 1f;
+            float kP = 1f / 100f;
+            float kI = kP / 2f;
+            float kD = 0f;
+            pitchPID = new PID32(kP, kI, kD, -maxValue, maxValue);
+            rollPID = new PID32(kP, kI, kD, -maxValue, maxValue);
         }
 
         public void OnUnregistered(FBWHostBase host)
@@ -29,40 +33,40 @@ namespace AutotrimTweak
 
         public void OnProcessCtrlState(ref FSInputState data, Vehicle vehicle)
         {
+            if (vehicle.Autotrim.enabled)
+            {
+                return;
+            }
+            //State
             float currentTime = Time.realtimeSinceStartup;
             float deltaTime = currentTime - lastTime;
             float pitchAxis = InputSettings.Axis_Pitch.GetAxis();
             float rollAxis = InputSettings.Axis_Roll.GetAxis();
-            float currentPitch = GetVehiclePitch(vehicle);
-            float currentRoll = GetVehicleRoll(vehicle);
-            float pitchError = currentPitch - lastPitchInput;
-            float rollError = currentRoll - lastRollInput;
+
+            //Rotation, eulerAngles is in degrees
+            Vector3 unrotated = vehicle.Rb.rotation.Inverse() * vehicle.Physics.AngularVelocity;
+            //Get angles
+            float pitch = unrotated.x;
+            float roll = unrotated.z;
+            float yaw = unrotated.y;
+
+            //Normalise to degrees per second
+            pitch = pitch / deltaTime;
+            roll = roll / deltaTime;
+            yaw = yaw / deltaTime;
+
+            //Don't update the PID loops if there is control input
             if (Math.Abs(pitchAxis) < 0.05d && Math.Abs(rollAxis) < 0.05d)
             {
-                lastPitchPIDOutput = Mathf.Clamp(pitchPID.Loop(0, pitchError, deltaTime, 0), -0.2f, 0.2f);
-                lastRollPIDOutput = Mathf.Clamp(rollPID.Loop(0, rollError, deltaTime, 0), -0.2f, 0.2f);
+                //What the fuck is s? It seems to be some delta offset for D, so no fucking idea.
+                lastPitchPIDOutput = Mathf.Clamp(pitchPID.Loop(0f, -pitch, deltaTime, 0), pitchPID.iMin, pitchPID.iMax);
+                lastRollPIDOutput = Mathf.Clamp(rollPID.Loop(0f, -roll, deltaTime, 0), rollPID.iMin, rollPID.iMax);
             }
-            Debug.Log($"{currentPitch} {pitchError} {lastPitchPIDOutput}");
-            //Apply changes
-            data.pitch -= lastPitchPIDOutput;
-            data.roll += lastRollPIDOutput;
-            //Save state data
-            lastPitchInput = currentPitch;
-            lastRollInput = currentRoll;
-            lastTime = currentTime;
-        }
 
-        private float GetVehiclePitch(Vehicle vehicle)
-        {
-            return FSControlUtil.GetVehiclePitch(vehicle) * Mathf.Rad2Deg;
-        }
-        private float GetVehicleYaw(Vehicle vehicle)
-        {
-            return FSControlUtil.GetVehicleYaw(vehicle) * Mathf.Rad2Deg;
-        }
-        private float GetVehicleRoll(Vehicle vehicle)
-        {
-            return FSControlUtil.GetVehicleRoll(vehicle) * Mathf.Rad2Deg;
+            //Apply changes
+            data.pitch += lastPitchPIDOutput;
+            data.roll += lastRollPIDOutput;
+            lastTime = currentTime;
         }
     }
 }
